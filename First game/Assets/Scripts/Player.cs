e074feea612945d95c6c59bd225a0e93ec8dade9
thomasgeerligs <thomasgeerligs@outlook.com>
2022-09-03 19:23:59 +0200

    Player animation + some code cleanups

--- First game/Assets/Scripts/Player.cs
+++ First game/Assets/Scripts/Player.cs
@@ -7,32 +7,32 @@ public class Player : MonoBehaviour
     //Movement section
     //This is all put in fixed update, nothing has to be saved here either.
     public float JumpHeight, MovementSpeed;
-    //public Animator MovementAnimation;
+    public Animator MovementAnimation;
     public static bool GroundCheck;
     public Rigidbody2D Rigidbody;
     public GameObject PlayerObject;
-    //bool WalkingLeft, WalkingRight;
+    bool WalkingLeft, WalkingRight;
     void FixedUpdate()
     {
         if (Input.GetKey("d"))
         {
             Rigidbody.velocity = new Vector2(+MovementSpeed, Rigidbody.velocity.y);
-            //WalkingRight = true;
-            //WalkingLeft = false;
+            WalkingRight = true;
+            WalkingLeft = false;
         }
         else
         {
             if (Input.GetKey("a"))
             {
                 Rigidbody.velocity = new Vector2(-MovementSpeed, Rigidbody.velocity.y);
-                //WalkingRight = false;
-                //WalkingLeft = true;
+                WalkingRight = false;
+                WalkingLeft = true;
             }
             else
             {
                 Rigidbody.velocity = new Vector2(0, Rigidbody.velocity.y);
-                //WalkingRight = false;
-                //WalkingLeft = false;
+                WalkingRight = false;
+                WalkingLeft = false;
             }
         }
         if (Input.GetKey("space") && GroundCheck == true)
@@ -50,23 +50,23 @@ public class Player : MonoBehaviour
         }
 
         //Animations
-        //if (WalkingRight == true)
-        //{
-        //    MovementAnimation.SetBool("WalkingLeft", false);
-        //    MovementAnimation.SetBool("WalkingRight", true);
-        //    MovementAnimation.SetBool("Idle", false);
-        //}
-        //if (WalkingLeft == true)
-        //{
-        //    MovementAnimation.SetBool("WalkingLeft", true);
-        //    MovementAnimation.SetBool("WalkingRight", false);
-        //    MovementAnimation.SetBool("Idle", false);
-        //}
-        //if (WalkingLeft == false && WalkingRight == false)
-        //{
-        //    MovementAnimation.SetBool("WalkingLeft", false);
-        //    MovementAnimation.SetBool("WalkingRight", false);
-        //    MovementAnimation.SetBool("Idle", true);
-        //}
+        if (WalkingRight == true)
+        {
+           MovementAnimation.SetBool("WalkingLeft", false);
+           MovementAnimation.SetBool("WalkingRight", true);
+           MovementAnimation.SetBool("Idle", false);
+        }
+        if (WalkingLeft == true)
+        {
+           MovementAnimation.SetBool("WalkingLeft", true);
+           MovementAnimation.SetBool("WalkingRight", false);
+           MovementAnimation.SetBool("Idle", false);
+        }
+        if (WalkingLeft == false && WalkingRight == false)
+        {
+           MovementAnimation.SetBool("WalkingLeft", false);
+           MovementAnimation.SetBool("WalkingRight", false);
+           MovementAnimation.SetBool("Idle", true);
+        }
     }
 }
